93b5346abc6a1a5f5c49ae0605c88c36e16490a4
Your Name <you@example.com>
2025-10-28 16:24:34 +0100

    yuhasue

--- action.c
+++ action.c
@@ -14,29 +14,8 @@
 
 void	safe_usleep(long duration_ms, t_philo *rules);
 long	get_time_ms(void);
-void	take_forks(t_philo *philos);
 void	action_print(t_philo *philos, const char *action);
 int		cleanup(t_philo *philos);
-
-void	action_eat(t_philo *philos)
-{
-	take_forks(philos);
-	pthread_mutex_lock(&philos->check_mutex);
-	if (get_time_ms() - philos->last_meal >= philos->rules->time_to_die)
-	{
-		pthread_mutex_unlock(philos->left_fork);
-		pthread_mutex_unlock(philos->right_fork);
-		pthread_mutex_unlock(&philos->check_mutex);
-		return ;
-	}
-	philos->last_meal = get_time_ms();
-	philos->meals_eaten += 1;
-	pthread_mutex_unlock(&philos->check_mutex);
-	action_print(philos, "is eating");
-	safe_usleep(philos->rules->time_to_eat, philos);
-	pthread_mutex_unlock(philos->left_fork);
-	pthread_mutex_unlock(philos->right_fork);
-}
 
 void	safe_usleep(long sleeptime_ms, t_philo *philos)
 {
@@ -57,39 +36,6 @@ long	get_time_ms(void)
 
 	gettimeofday(&tv, NULL);
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
-}
-
-void	take_forks(t_philo *philos)
-{
-	if (philos->id % 2 != 0)
-		usleep(1000);
-	if (philos->id == philos->rules->num_philos)
-		usleep(1000);
-	if (philos->id % 2 == 0)
-	{
-		pthread_mutex_lock(philos->left_fork);
-		action_print(philos, "has taken a fork");
-		pthread_mutex_lock(philos->right_fork);
-	}
-	else
-	{
-		pthread_mutex_lock(philos->right_fork);
-		action_print(philos, "has taken a fork");
-		pthread_mutex_lock(philos->left_fork);
-	}
-	action_print(philos, "has taken a fork");
-}
-
-void	action_print(t_philo *philos, const char *action)
-{
-	long	timestamp;
-
-	if (!alive(philos))
-		return ;
-	timestamp = (get_time_ms() - philos->rules->start_time);
-	pthread_mutex_lock(&philos->rules->print_mutex);
-	printf("%ld %d %s\n", timestamp, philos->id + 1, action);
-	pthread_mutex_unlock(&philos->rules->print_mutex);
 }
 
 int	cleanup(t_philo *philos)
--- routine.c
+++ routine.c
@@ -12,6 +12,12 @@
 
 #include "header.h"
 
+void	*philos_routine(void *arg);
+void	action_eat(t_philo *philos);
+void	take_forks(t_philo *philos);
+void	action_print(t_philo *philos, const char *action);
+
+
 void	*philos_routine(void *arg)
 {
 	t_philo	*philos;
@@ -21,8 +27,6 @@ void	*philos_routine(void *arg)
 	philos->last_meal = philos->rules->start_time;
 	pthread_mutex_unlock(&philos->check_mutex);
 	action_print(philos, "thinking");
-	if (philos->id % 2 != 0)
-		ft_sleep(philos->rules->time_to_eat);
 	while (alive(philos))
 	{
 		action_eat(philos);
@@ -31,4 +35,59 @@ void	*philos_routine(void *arg)
 		action_print(philos, "thinking");
 	}
 	return (NULL);
+}
+
+
+void	action_eat(t_philo *philos)
+{
+	take_forks(philos);
+	pthread_mutex_lock(&philos->check_mutex);
+	if (get_time_ms() - philos->last_meal >= philos->rules->time_to_die)
+	{
+		pthread_mutex_unlock(philos->left_fork);
+		pthread_mutex_unlock(philos->right_fork);
+		pthread_mutex_unlock(&philos->check_mutex);
+		return ;
+	}
+	philos->last_meal = get_time_ms();
+	philos->meals_eaten += 1;
+	pthread_mutex_unlock(&philos->check_mutex);
+	action_print(philos, "is eating");
+	safe_usleep(philos->rules->time_to_eat, philos);
+	pthread_mutex_unlock(philos->left_fork);
+	pthread_mutex_unlock(philos->right_fork);
+}
+
+
+void	take_forks(t_philo *philos)
+{
+	if (philos->id % 2 != 0)
+		usleep(1000);
+	if (philos->id == philos->rules->num_philos)
+		usleep(1000);
+	if (philos->id % 2 == 0)
+	{
+		pthread_mutex_lock(philos->left_fork);
+		action_print(philos, "has taken a fork");
+		pthread_mutex_lock(philos->right_fork);
+	}
+	else
+	{
+		pthread_mutex_lock(philos->right_fork);
+		action_print(philos, "has taken a fork");
+		pthread_mutex_lock(philos->left_fork);
+	}
+	action_print(philos, "has taken a fork");
+}
+
+void	action_print(t_philo *philos, const char *action)
+{
+	long	timestamp;
+
+	if (!alive(philos))
+		return ;
+	timestamp = (get_time_ms() - philos->rules->start_time);
+	pthread_mutex_lock(&philos->rules->print_mutex);
+	printf("%ld %d %s\n", timestamp, philos->id + 1, action);
+	pthread_mutex_unlock(&philos->rules->print_mutex);
 }
